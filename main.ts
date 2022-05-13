fa117c7c1355949e62f00ee2230be8414991d3ee
Brandon <Brandonsexton14@gmail.com>
2022-05-13 10:45:54 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,31 @@
+let Motor_Forward = 0
+let Motor_Backward = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 20) {
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            . # # . .
+            . . . . .
+            . # . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(20)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(21)
+})
 basic.forever(function () {
     radio.setGroup(1)
     radio.setTransmitPower(6)
     radio.setFrequencyBand(80)
+    Motor_Forward = 1
+    Motor_Backward = 2
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P3) == 0) {
+    if (pins.digitalReadPin(DigitalPin.P3) == Motor_Forward) {
         radio.sendNumber(0)
     }
 })
@@ -14,7 +35,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P4) == 0) {
+    if (pins.digitalReadPin(DigitalPin.P4) == Motor_Backward) {
         radio.sendNumber(0)
     }
 })
@@ -24,17 +45,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P5) == 0) {
-        radio.sendNumber(0)
-    }
-})
-basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P6) == 0) {
-        radio.sendNumber(0)
-    }
-})
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P5) == 0) {
         radio.sendNumber(0)
     }
 })
