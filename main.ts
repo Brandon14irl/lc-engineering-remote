94e276981cbd8ed7a3008144b28f54ca2172ecfb
Brandon <Brandonsexton14@gmail.com>
2022-05-13 20:51:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
 basic.forever(function () {
     radio.setGroup(1)
-    radio.setTransmitPower(6)
-    radio.setFrequencyBand(80)
+    basic.showLeds(`
+        # # . . .
+        # . # . .
+        # . # . .
+        # # . . .
+        # . # . .
+        `)
 })
