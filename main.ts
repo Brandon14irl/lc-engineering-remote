f9596ce725e550e0c735e76a57647846db77dc34
Brandon <Brandonsexton14@gmail.com>
2022-05-15 22:07:03 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,46 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
+basic.forever(function () {
+    radio.setGroup(80)
+    led.enable(false)
 })
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(3)
+// push to break button up
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P3) == 0) {
+        radio.sendNumber(1)
+    }
 })
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
+// push to break button left
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P4) == 0) {
+        radio.sendNumber(2)
+    }
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.sendNumber(3)
+// on off switch
+// 
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+        radio.sendNumber(5)
+    }
+})
+// push to break button right
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P6) == 0) {
+        radio.sendNumber(3)
+    }
+})
+// bumper button left
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P9) == 1) {
+        radio.sendNumber(6)
+    }
+})
+// push to break button down
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P7) == 0) {
+        radio.sendNumber(4)
+    }
 })
 basic.forever(function () {
-    radio.setGroup(1)
-    basic.showLeds(`
-        # # # # .
-        # . . # .
-        # # # . .
-        # . . # .
-        # . . # .
-        `)
+    if (pins.digitalReadPin(DigitalPin.P10) == 1) {
+        radio.sendNumber(7)
+    }
 })
