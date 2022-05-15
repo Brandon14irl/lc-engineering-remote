f1ea4daa7eecc878bbe8b1e6d6a46218a4ab8c11
Brandon <Brandonsexton14@gmail.com>
2022-05-15 22:11:56 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,23 +14,17 @@ basic.forever(function () {
         radio.sendNumber(2)
     }
 })
-// on off switch
-// 
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P8) == 1) {
-        radio.sendNumber(5)
-    }
-})
 // push to break button right
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P6) == 0) {
         radio.sendNumber(3)
     }
 })
-// bumper button left
+// on off switch
+// 
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P9) == 1) {
-        radio.sendNumber(6)
+    if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+        radio.sendNumber(5)
     }
 })
 // push to break button down
@@ -39,8 +33,15 @@ basic.forever(function () {
         radio.sendNumber(4)
     }
 })
+// bumper right
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P10) == 1) {
         radio.sendNumber(7)
+    }
+})
+// bumper button left
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P9) == 1) {
+        radio.sendNumber(6)
     }
 })
